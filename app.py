3d280b4b6bd1083fd44d46bc668abb9ec669406e
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2023-11-24 13:31:23 -0700

    fuck yeah
    wooo  yeah

--- app.py
+++ app.py
@@ -17,5 +17,19 @@ def click():
     # This is just a simple example to return some data on click
     return "Button Clicked motherfucker!"
 
+
+@app.route('/tab1')
+def tab1():
+    return render_template('html/tab1.html')
+
+@app.route('/tab2')
+def tab2():
+    return render_template('html/tab2.html')
+
+@app.route('/tab3')
+def tab3():
+    return render_template('html/tab3.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
