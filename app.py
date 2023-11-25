7d6cf59ce3b62abf5047d3cf6e3c658bcf489c07
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2023-11-24 21:01:40 -0700

    image slideshow doesn't work

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, jsonify
+import itertools
 import os
 
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'), static_folder='static')
+
+images = ['../../static/images/rocketpic1.JPG', '../../static/images/rocketpie2.JPG', '../../static/images/skateboard1.JPG']
 
 @app.route('/')
 
@@ -11,6 +14,21 @@ def index():
     print(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
 
     return render_template('index.html')
+
+
+@app.route('/images/logo.png')
+def download_file(filename):
+    return send_from_directory('static/images', filename)
+
+@app.route('/get_images')
+def get_images():
+    image_data = []
+    image_folder = 'images'
+    for filename in os.listdir('static/images'):
+        if filename.endswith(".jpg") or filename.endswith(".png"):  # Filter by image extension
+            image_data.append({'filename': filename})
+    return jsonify(image_data)
+
 
 @app.route('/click', methods=['GET'])
 def click():
